ada6d504d73051b16ff72f467ee46c32ee1ce6db
7rencong <dkko9938@gmail.com>
2025-07-07 01:11:17 +0700

    Add files via upload

---
+++ tele.php
@@ -0,0 +1,8 @@
+<?php 
+$id_bot = "7591119123:AAEpkEQTR-Ewc-4ypMb6wkVG8wKS5l5TQKU"; // Bot Baru -> Add @BotFather, Klik Start, Kasih nama bot, copy Access Token lalu paste disini
+$telegram_id   = "5905674236"; // Ganti ChatId -> Add @chatIDrobot di telegram, klik start, copy chatId lalu Paste disini || KLIK START PADA BOT @KorbanTeleBot
+
+// Note, Akun telegram kamu harus Join/Bergabung ke BOT yang kamu buat
+
+
+?>
